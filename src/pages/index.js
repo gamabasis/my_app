2e2860ef7c2d20b957cbf91e527bbafa539c5514
Juan Gamboa <juan.gamboa.abarca@gmail.com>
2023-05-09 12:23:20 -0600

    otro cambio a index

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
         div className = "z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex" >
         <
         p className = "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
-        Get started by editing & nbsp; <
-        code className = "font-mono font-bold" > src / pages / index.js < /code> <
-        /p> <
+        Saludos! <
+        code className = "font-mono font-bold" > src / pages / index.js < /code> < /
+        p > <
         div className = "fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" >
         <
         a className = "pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -28,8 +28,8 @@ export default function Home() {
         priority /
         >
         <
-        /a> <
-        /div> <
+        /a> < /
+        div > <
         /div>
 
         <
@@ -57,12 +57,12 @@ export default function Home() {
         Docs { ' ' } <
         span className = "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none" >
         - & gt; <
-        /span> <
-        /h2> <
+        /span> < /
+        h2 > <
         p className = { `m-0 max-w-[30ch] text-sm opacity-50` } >
         Find in -depth information about Next.js features and API. <
-        /p> <
-        /a>
+        /p> < /
+        a >
 
         <
         a href = "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -74,13 +74,13 @@ export default function Home() {
         Learn { ' ' } <
         span className = "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none" >
         - & gt; <
-        /span> <
-        /h2> <
+        /span> < /
+        h2 > <
         p className = { `m-0 max-w-[30ch] text-sm opacity-50` } >
         __Learn about Next.js in an interactive course with & nbsp; quizzes!
         <
-        /p> <
-        /a>
+        /p> < /
+        a >
 
         <
         a href = "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -92,12 +92,12 @@ export default function Home() {
         Templates { ' ' } <
         span className = "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none" >
         - & gt; <
-        /span> <
-        /h2> <
+        /span> < /
+        h2 > <
         p className = { `m-0 max-w-[30ch] text-sm opacity-50` } >
         Discover and deploy boilerplate example Next.js & nbsp; projects. <
-        /p> <
-        /a>
+        /p> < /
+        a >
 
         <
         a href = "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -109,13 +109,13 @@ export default function Home() {
         Deploy { ' ' } <
         span className = "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none" >
         - & gt; <
-        /span> <
-        /h2> <
+        /span> < /
+        h2 > <
         p className = { `m-0 max-w-[30ch] text-sm opacity-50` } >
         Instantly deploy your Next.js site to a shareable URL with Vercel. <
-        /p> <
-        /a> <
-        /div> <
-        /main>
+        /p> < /
+        a > <
+        /div> < /
+        main >
     )
 }
